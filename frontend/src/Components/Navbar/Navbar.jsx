2e69287d034c1d9f4e22bf02d30aac22ebb9b9f3
Sandeep Gupta <sandeepgupta97235@gmail.com>
2026-08-18 23:04:31 +0530

    add a line in Navbar at Bottom.

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -4,31 +4,33 @@ import { FaSearch } from "react-icons/fa";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { CgProfile } from "react-icons/cg";
 
-const Navbar = ({setShowSignUp}) => {
+const Navbar = ({ setShowSignUp }) => {
   return (
-    <div className='navbar'>
-      <h1 className='main-logo-text'>ShopSphere</h1>
+    <div className="navbar-main">
 
-      <div className="serach-function">
-        <div className="search-icon-div">
-          <FaSearch className='serach-icon' />
-        </div>
-        <input className='search-box' type="text" name="" id="" placeholder='Search here...' />
-      </div>
+      <div className='navbar'>
+        <h1 className='main-logo-text'>ShopSphere</h1>
 
-      <div className="main-right">
-        <CgProfile className='Profile-icon'/>
-        <div className="cart-section">
-          <div className="cart-icon">
-            <AiOutlineShoppingCart />
+        <div className="serach-function">
+          <div className="search-icon-div">
+            <FaSearch className='serach-icon' />
           </div>
-          {/* <p>Cart</p> */}
+          <input className='search-box' type="text" name="" id="" placeholder='Search here...' />
         </div>
-        <button onClick={()=>setShowSignUp(true)}>Login</button>
+
+        <div className="main-right">
+          <CgProfile className='Profile-icon' />
+          <div className="cart-section">
+            <div className="cart-icon">
+              <AiOutlineShoppingCart />
+            </div>
+            {/* <p>Cart</p> */}
+          </div>
+          <button onClick={() => setShowSignUp(true)}>Login</button>
+        </div>
       </div>
-
-
-    </div>
+      <hr />
+   </div>
   )
 }
 
